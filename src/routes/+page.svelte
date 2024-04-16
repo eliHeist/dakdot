f9dee41a675d244b754c514f7b048b6f41fb3222
Elijah Triumph Angengeun <eliyang256@outlook.com>
2024-04-16 19:10:43 +0300

    nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="breakout">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style lang="scss">
+    $red: #f11;
+    h1 {
+        color: $red;
+    }
+
+    p{
+        a{
+            color: $red;
+        }
+    }
+</style>
